f22b818e6e1ee876cc96c0e1d40df70eb9b06d9f
Titouaaaan <titouanguerin@gmail.com>
2025-03-26 11:41:35 +0100

    Added logout button

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { signOut } from 'next-auth/react';
 
 const Footer = () => {
   return (
     <footer style={styles.footer}>
       <p>&copy; {new Date().getFullYear()} Chess Coach App.</p>
+      <button onClick={() => signOut()}>Logout</button>
     </footer>
   );
 };
